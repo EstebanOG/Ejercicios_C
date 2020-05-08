e2103641722e7727f80ba6857215f3eb86e97a86
Esteban <juanesteban199911@gmail.com>
2020-05-08 08:51:36 -0500

    Se inicia ejercicio

---
+++ Trapecio/Trapecio.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+void leerDatos();
+void calcularArea();
+void calcularPerimetro();
+void calcularLado();
+int main(int argc, char** argv) {
+	int baseMayor, baseMenor, altura;
+	return 0;
+}
