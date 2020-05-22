92dbc60efcde55f639c21f4b2677a75aa9368fcb
Esteban <juanesteban199911@gmail.com>
2020-05-22 09:14:39 -0500

    Se inicia ejercicio palindromo

---
+++ palindromo/palindromo.cpp
@@ -0,0 +1,30 @@
+#include "palindromo.h"
+
+int main()
+{
+    setlocale(LC_ALL, "spanish");
+    string texto;
+    string* palabrasAlDerecho;
+    string* palabrasAlReves;
+    string* palindromos;
+    int contador, contadorPalindromos;
+    leerTexto(texto);
+    contador=contadorDePalabras(texto);
+    memoria(palabrasAlDerecho, contador);
+    formarPalabrasAlDerecho( palabrasAlDerecho, texto);
+    memoria(palabrasAlReves, contador);
+    formarPalabrasAlReves(palabrasAlDerecho, palabrasAlReves, contador);
+    //
+    contadorPalindromos = contarPalindromos(palabrasAlDerecho, palabrasAlReves, contadorPalindromos);
+    memoria( palindromos, contadorPalindromos);
+    crearArregloDePalindromos(palindromos, palabrasAlDerecho, formarPalabrasAlReves, contador);
+    print(texto);
+    print(palabrasAlDerecho, contador);
+    print(palindromos, contadorPalindromos);
+    liberarMemoria(palabrasAlDerecho);
+    liberarMemoria(palabrasAlReves);
+    liberarMemoria(palindromos);
+
+    system("pause");
+    
+}
